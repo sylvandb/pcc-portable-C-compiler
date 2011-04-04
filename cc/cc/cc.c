80168fba89732de2f7d968d6bd35e2788b5e8db9
ragge <ragge>
2011-04-04 14:50:23 +0000

    Update cc.c 1.177 and cc.1 1.17 from mainline.

--- cc/cc/cc.c
+++ cc/cc/cc.c
@@ -487,7 +487,10 @@ main(int argc, char *argv[])
 				break;
 
 			case 'g': /* create debug output */
-				gflag++;
+				if (argv[i][2] == '0')
+					gflag = 0;
+				else
+					gflag++;
 				break;
 
 			case 'i':
